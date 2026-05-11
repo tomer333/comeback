e62bb16bb77f091774515954369ccfdf63903c18
Tomer Dahan <tomer.dahan01@post.runi.ac.il>
2026-05-11 21:50:46 +0300

    Fix base path bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/comeback/' : '/',
   server: {
     host: "::",
     port: 8080,
